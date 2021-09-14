dc6ec4c407f9ae0abba1284f9f9903c536699c5c
Brittany Richards <brittany.m.richards3@gmail.com>
2021-09-14 10:02:13 -0500

    added searchresultspage

--- src/main/java/com/codeup/adlister/controllers/SearchAdsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/SearchAdsServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +13,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-
+@WebServlet(name = "controllers.SearchAdsServlet", urlPatterns = "/ads/search")
 public class SearchAdsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String keyword = request.getParameter("keyword");
@@ -21,7 +22,7 @@ public class SearchAdsServlet extends HttpServlet {
         boolean inputHasErrors = keyword.isEmpty();
 
         if (inputHasErrors) {
-            response.sendRedirect("/profile");//NEED A PAGE TO REDIRECT TO!
+            response.sendRedirect("/ads");//NEED A PAGE TO REDIRECT TO!
             return;
         }
     }
--- src/main/webapp/WEB-INF/ads/search.jsp
+++ src/main/webapp/WEB-INF/ads/search.jsp
@@ -9,20 +9,15 @@
 <body>
 <jsp:include page="/WEB-INF/partials/navbar.jsp" />
 <div class="container">
-    <h1>Search Through Ads</h1>
-    <form action="/ads/search" method="POST">
-        <div class="form-group">
-            <label for="title">Ad Title</label>
-            <input id="title" name="title" class="form-control" type="text">
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <input id="description" name="description" class="form-control" type="text">
-        </div>
-        <input type="submit" class="btn btn-primary btn-block" value="Search">
-    </form>
-</div>
+    <h1>Here Are all the ads!</h1>
 
+    <c:forEach var="ad" items="${ads}">
+        <div class="col-md-6">
+            <h2>${ad.title}</h2>
+            <p>${ad.description}</p>
+        </div>
+    </c:forEach>
+</div>
 
 </body>
 </html>
